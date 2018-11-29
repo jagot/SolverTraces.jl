64af1157b573105122fd8a12006b3e0cc8551e81
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-11-29 14:14:59 +0100

    Precalculate print interval

--- src/solver_trace.jl
+++ src/solver_trace.jl
@@ -3,7 +3,7 @@ import Base: push!
 mutable struct SolverTrace
     num_steps::Int
     i::Int
-    num_printouts::Int
+    print_interval::Int
     progress::Progress
     columns::Vector{TraceColumn}
 end
@@ -12,11 +12,13 @@ function SolverTrace(num_steps::Int,
                      columns::TraceColumn...;
                      num_printouts::Integer=min(num_steps,10),
                      kwargs...)
+    print_interval = num_printouts > num_steps ? 1 : num_steps÷num_printouts
+
     columns = Vector{TraceColumn}([columns...])
     isempty(columns) && push!(columns, CurrentStep(num_steps))
 
     progress = Progress(num_steps; kwargs...)
-    SolverTrace(num_steps, 0, num_printouts, progress, columns)
+    SolverTrace(num_steps, 0, print_interval, progress, columns)
 end
 
 push!(trace::SolverTrace, column::TraceColumn) =
@@ -30,7 +32,7 @@ end
 
 function next!(s::SolverTrace)
     s.i += 1
-    if s.i%(s.num_steps÷s.num_printouts) == 0 || s.i==1
+    if s.i%s.print_interval == 0 || s.i==1
         # Carriage return & clearing has to be done on stderr since
         # ProgressMeter prints to that stream; this is important on
         # Windows.
