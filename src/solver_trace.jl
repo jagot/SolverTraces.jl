1de06fecc032fe6cce5c146865ed611d91aabf4c
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-11-29 14:17:23 +0100

    Added possibility of disabling progress meter

--- src/solver_trace.jl
+++ src/solver_trace.jl
@@ -1,23 +1,28 @@
 import Base: push!
 
-mutable struct SolverTrace
+mutable struct SolverTrace{P}
     num_steps::Int
     i::Int
     print_interval::Int
-    progress::Progress
+    progress::P
     columns::Vector{TraceColumn}
 end
 
 function SolverTrace(num_steps::Int,
                      columns::TraceColumn...;
                      num_printouts::Integer=min(num_steps,10),
+                     progress_meter::Bool=true,
                      kwargs...)
     print_interval = num_printouts > num_steps ? 1 : num_steps÷num_printouts
 
     columns = Vector{TraceColumn}([columns...])
     isempty(columns) && push!(columns, CurrentStep(num_steps))
 
-    progress = Progress(num_steps; kwargs...)
+    progress = if progress_meter
+        Progress(num_steps; kwargs...)
+    else
+        nothing
+    end
     SolverTrace(num_steps, 0, print_interval, progress, columns)
 end
 
@@ -30,6 +35,9 @@ function print_header(s::SolverTrace)
     println(crayon"underline", join(map(column -> column.header, s.columns), " "), crayon"reset")
 end
 
+next!(::Nothing) = nothing
+next!(progress::Progress) = ProgressMeter.next!(progress)
+
 function next!(s::SolverTrace)
     s.i += 1
     if s.i%s.print_interval == 0 || s.i==1
@@ -39,7 +47,7 @@ function next!(s::SolverTrace)
         clear_current_line(stderr)
         println(join(map(column -> format(column.fmt, column(s.i)...), s.columns), " "))
     end
-    ProgressMeter.next!(s.progress)
+    next!(s.progress)
 end
 
 export SolverTrace, print_header
