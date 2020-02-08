4d02a387316b5999fc4d97fa3897419d48abec8f
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-02-08 16:45:03 +0100

    Potentially fixed escaped string comparisons on Windows

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,12 @@ using SolverTraces
 using Formatting
 using Test
 
+stripesc(a::T) where {T<:String} = replace(a, r"\e\[(.*?)m" => "")
+
+# We use this to compare escaped strings until
+# https://github.com/JuliaLang/julia/issues/31491 is resolved
+is_esc_str_equal = @static Sys.iswindows() ? ((a,b) -> stripesc(a) == stripesc(b)) : isequal
+
 include("colors.jl")
 include("current_step.jl")
 include("performance.jl")
--- test/tolerance.jl
+++ test/tolerance.jl
@@ -13,29 +13,31 @@ end
     test_base_exp(0.1, 1.0, -1)
     test_base_exp(-0.01π, -π, -2)
 
+    # Need to fix tests for Windows
+
     t = Tolerance(1e-3)
     t.current=1.0
-    @test format(t, 1) == "\e[38;2;255;0;0m1.00×10⁰  \e[0m(10⁻³)"
+    @test is_esc_str_equal(format(t, 1), "\e[38;2;255;0;0m1.00×10⁰  \e[0m(10⁻³)")
 
     t.current=1e-1
-    @test format(t, 1) == "\e[38;2;255;170;0m1.00×10⁻¹ \e[0m(10⁻³)"
+    @test is_esc_str_equal(format(t, 1), "\e[38;2;255;170;0m1.00×10⁻¹ \e[0m(10⁻³)")
 
     t.current=1e-2
-    @test format(t, 1) == "\e[38;2;170;255;0m1.00×10⁻² \e[0m(10⁻³)"
+    @test is_esc_str_equal(format(t, 1), "\e[38;2;170;255;0m1.00×10⁻² \e[0m(10⁻³)")
 
     t.current=1e-3
-    @test format(t, 1) == "\e[38;2;0;255;0m1.00×10⁻³ \e[0m(10⁻³)"
+    @test is_esc_str_equal(format(t, 1), "\e[38;2;0;255;0m1.00×10⁻³ \e[0m(10⁻³)")
     
     t = Tolerance(1e-3, print_target=false)
     t.current=1.0
-    @test format(t, 1) == "\e[38;2;255;0;0m1.00×10⁰  \e[0m"
+    @test is_esc_str_equal(format(t, 1), "\e[38;2;255;0;0m1.00×10⁰  \e[0m")
 
     t.current=1e-1
-    @test format(t, 1) == "\e[38;2;255;170;0m1.00×10⁻¹ \e[0m"
+    @test is_esc_str_equal(format(t, 1), "\e[38;2;255;170;0m1.00×10⁻¹ \e[0m")
 
     t.current=1e-2
-    @test format(t, 1) == "\e[38;2;170;255;0m1.00×10⁻² \e[0m"
+    @test is_esc_str_equal(format(t, 1), "\e[38;2;170;255;0m1.00×10⁻² \e[0m")
 
     t.current=1e-3
-    @test format(t, 1) == "\e[38;2;0;255;0m1.00×10⁻³ \e[0m"
+    @test is_esc_str_equal(format(t, 1), "\e[38;2;0;255;0m1.00×10⁻³ \e[0m")
 end
